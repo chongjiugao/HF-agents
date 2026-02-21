ced3dbac7a47e3f82784e548347fab9598918a85
chongjiu <chongjiugao@gmail.com>
2026-02-20 21:01:57 -0800

    feat: run mcp with local agent

--- local.py
+++ local.py
@@ -1,17 +1,21 @@
 # Sigh, I keep hitting the inference quota on HuggingFace.
 # So here we go, let's run this locally with Ollama :)
+import os
 from smolagents import LiteLLMModel
-from smolagents.models import ChatMessage
+# from smolagents.models import ChatMessage
 from smolagents import Tool, CodeAgent
+from smolagents import ToolCollection, CodeAgent
+from mcp import StdioServerParameters
 
+# Initialize the model
 model = LiteLLMModel(
     model_id="ollama_chat/qwen2:7b",  # Or try other Ollama-supported models
     api_base="http://127.0.0.1:11434",  # Default Ollama local server
     num_ctx=8192,
 )
 
-
-# Creating a class for tool
+# How do we create a tool??
+# Apparoach 1: Make a class that inherits Tool
 class SuperheroPartyThemeTool(Tool):
     name = "superhero_party_theme_generator"
     description = """
@@ -36,13 +40,28 @@ class SuperheroPartyThemeTool(Tool):
 
         return themes.get(category.lower(), "Themed party idea not found. Try 'classic heroes', 'villain masquerade', or 'futuristic Gotham'.")
 
-# Instantiate the tool
-party_theme_tool = SuperheroPartyThemeTool()
-agent = CodeAgent(tools=[party_theme_tool], model=model)
-
-# Run the agent to generate a party theme idea
-result = agent.run(
-    "What would be a good superhero party idea for a 'villain masquerade' theme?"
+# Approach 2: load agent tools from MCP
+server_parameters = StdioServerParameters(
+    command="uvx",
+    args=["--quiet", "pubmedmcp@0.1.3"],
+    env={"UV_PYTHON": "3.12", **os.environ},
 )
 
-print(result) 
+
+def main():
+    # Approach 1: use the tool directly 
+
+    # party_theme_tool = SuperheroPartyThemeTool()
+    # agent = CodeAgent(tools=[party_theme_tool], model=model)
+    # result = agent.run(
+    #     "What would be a good superhero party idea for a 'villain masquerade' theme?"
+    # )
+    # print(result)
+
+    # Approach 2: use the pubmed MCP tool collection
+    with ToolCollection.from_mcp(server_parameters, trust_remote_code=True) as tool_collection:
+        agent = CodeAgent(tools=[*tool_collection.tools], model=model, add_base_tools=True)
+        agent.run("Please find a remedy for hangover.")
+
+if __name__ == "__main__":
+    main()
